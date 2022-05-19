628461af4adc26421b162b046bb8b5af652dacc4
seba <sebamgb@gmail.com>
2022-05-19 14:20:52 -0400

    herencia

--- src/main.go
+++ src/main.go
@@ -1,45 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type employee struct {
-	id       int
-	name     string
-	vacation bool
+type person struct {
+	name string
+	age  int
 }
-
-func newEmployee(id int, name string, vacation bool) *employee {
-	return &employee{
-		id:       id,
-		name:     name,
-		vacation: vacation,
-	}
+type employee struct {
+	id int
+}
+type fullTimeEmployee struct {
+	person
+	employee
 }
 
 func main() {
-	// forma 1
-	e := employee{}
-	fmt.Println(e)
-	// forma 2
-	e2 := employee{
-		id:       1,
-		name:     "Name",
-		vacation: true,
-	}
-	fmt.Println(e2)
-	// forma 3 (equivalente a forma 1)
-	//-instancia apunta al struct
-	e3 := new(employee)
-	fmt.Println(e3)  //imprime una referencia, mas explicita que la dirección de memoria
-	fmt.Println(*e3) //imprime el valor
-	//-explicita luego de haber instanciado
-	e3.id = 1
-	e3.name = "Name 3"
-	e3.vacation = true
-	fmt.Println(*e3)
-	// forma 4
-	e4 := newEmployee(4, "Name 4", true)
-	fmt.Printf("%v\n", *e4)
+	ftEmployee := fullTimeEmployee{}
+	ftEmployee.name = "Name"
+	ftEmployee.age = 22
+	ftEmployee.id = 1
+	fmt.Printf(" Full time employee %v\n", ftEmployee) //imprime claramente la composicion claramente la composición a la herencia que aplica go
 }
