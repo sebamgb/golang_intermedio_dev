c51de3e5bf824644f5d55e7345b1556f6126cd86
seba <sebamgb@gmail.com>
2022-05-19 13:38:14 -0400

    constructores

--- src/main.go
+++ src/main.go
@@ -5,38 +5,41 @@ import (
 )
 
 type employee struct {
-	id   int
-	name string
+	id       int
+	name     string
+	vacation bool
 }
 
-// en los structs los metodos se asignan utilizando receiver functions
-
-func (e *employee) setId(id int) {
-	e.id = id
-}
-
-func (e *employee) setName(name string) {
-	e.name = name
-}
-
-func (e employee) getId() int {
-	return e.id
-}
-
-func (e employee) getName() string {
-	return e.name
+func newEmployee(id int, name string, vacation bool) *employee {
+	return &employee{
+		id:       id,
+		name:     name,
+		vacation: vacation,
+	}
 }
 
 func main() {
+	// forma 1
 	e := employee{}
 	fmt.Println(e)
-	e.id = 1
-	e.name = "Name"
-	fmt.Println(e)
-	e.setId(5)
-	e.setName("Name 2")
-	fmt.Println(e)
-	fmt.Println()
-	fmt.Println(e.getId())
-	fmt.Println(e.getName())
+	// forma 2
+	e2 := employee{
+		id:       1,
+		name:     "Name",
+		vacation: true,
+	}
+	fmt.Println(e2)
+	// forma 3 (equivalente a forma 1)
+	//-instancia apunta al struct
+	e3 := new(employee)
+	fmt.Println(e3)  //imprime una referencia, mas explicita que la dirección de memoria
+	fmt.Println(*e3) //imprime el valor
+	//-explicita luego de haber instanciado
+	e3.id = 1
+	e3.name = "Name 3"
+	e3.vacation = true
+	fmt.Println(*e3)
+	// forma 4
+	e4 := newEmployee(4, "Name 4", true)
+	fmt.Printf("%v\n", *e4)
 }
