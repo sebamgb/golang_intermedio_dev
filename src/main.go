969d801dcae535d366aaf9e4ef3dc7847d952660
seba <sebamgb@gmail.com>
2022-05-18 21:30:36 -0400

    metodos en go

--- src/main.go
+++ src/main.go
@@ -9,10 +9,34 @@ type employee struct {
 	name string
 }
 
+// en los structs los metodos se asignan utilizando receiver functions
+
+func (e *employee) setId(id int) {
+	e.id = id
+}
+
+func (e *employee) setName(name string) {
+	e.name = name
+}
+
+func (e employee) getId() int {
+	return e.id
+}
+
+func (e employee) getName() string {
+	return e.name
+}
+
 func main() {
 	e := employee{}
 	fmt.Println(e)
 	e.id = 1
 	e.name = "Name"
 	fmt.Println(e)
+	e.setId(5)
+	e.setName("Name 2")
+	fmt.Println(e)
+	fmt.Println()
+	fmt.Println(e.getId())
+	fmt.Println(e.getName())
 }
